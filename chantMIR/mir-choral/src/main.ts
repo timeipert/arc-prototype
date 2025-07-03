23d1024012826e12a498eb6018c93720ae0e4989
timeipert <tim.eipert@gmail.com>
2025-07-03 15:20:04 +0200

    changed volpiano path

--- chantMIR/mir-choral/src/main.ts
+++ chantMIR/mir-choral/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import { createPinia } from 'pinia';
-import '@/assets/volpiano/volpiano.css';
+import '@/assets/volpiano.css';
 import App from './App.vue'
 
 
